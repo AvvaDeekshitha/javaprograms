28bac8cf130d05d897dc876ef3db52036ac4216b
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-08-07 10:37:58 +0530

    Update Taskone.java

--- Taskone.java
+++ Taskone.java
@@ -14,7 +14,7 @@ public class Taskone {
         if(n<=1)
             return false;
 
-        for(int i=2;i<=Math.sqrt(n);i++) {
+        for(int i=2;i<=(n);i++) {
             if(n%i==0)
                 return false;
         }
