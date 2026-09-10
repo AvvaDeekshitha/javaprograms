158fdc00e60b82a00f5c61940b8c98346ee48d76
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-10 21:05:22 +0530

    Create endswith.java

---
+++ assignment3/endswith.java
@@ -0,0 +1,10 @@
+class endswith {
+    public static void main(String[] args) {
+        String s = "Java Programming";
+
+        if (s.endsWith("ing"))
+            System.out.println("String ends with ing");
+        else
+            System.out.println("Does not end with ing");
+    }
+}
