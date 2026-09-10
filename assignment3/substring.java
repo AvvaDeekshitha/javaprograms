652717c1eee1e777e87f5d352b85d007c37cd4e4
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-10 20:55:20 +0530

    Create substring.java

---
+++ assignment3/substring.java
@@ -0,0 +1,8 @@
+class substring {
+    public static void main(String[] args) {
+        String s = "Programming";
+
+        System.out.println(s.substring(3));
+        System.out.println(s.substring(0, 6));
+    }
+}
