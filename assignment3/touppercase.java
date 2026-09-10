c47db942eca744ad30a6ffee99df7ab58592b0e7
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-10 20:52:08 +0530

    Add touppercase.java to demonstrate string case methods

---
+++ assignment3/touppercase.java
@@ -0,0 +1,8 @@
+class touppercase {
+    public static void main(String[] args) {
+        String s = "Java Programming";
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+}
