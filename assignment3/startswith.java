37fe5b1c2711a4a816c8bac9e0aee4ecb6639175
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-10 21:04:32 +0530

    Create startswith.java

---
+++ assignment3/startswith.java
@@ -0,0 +1,10 @@
+class startswith {
+    public static void main(String[] args) {
+        String s = "Java Programming";
+
+        if (s.startsWith("Java"))
+            System.out.println("String starts with Java");
+        else
+            System.out.println("Does not start with Java");
+    }
+}
