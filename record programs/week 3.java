2a65ca0ba8df40e4a89cbb603eccc6624d8c7973
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-11 08:09:03 +0530

    Update week 3.java

--- record programs/week 3.java
+++ record programs/week 3.java
@@ -1,39 +1,25 @@
 import java.util.Scanner;
-
-class PrimePalindrome {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        // Check prime
-        boolean prime = true;
-
-        if (n <= 1) {
-            prime = false;
-        } else {
-            for (int i = 2; i <= n / 2; i++) {
-                if (n % i == 0) {
-                    prime = false;
-                    break;
-                }
-            }
-        }
-
-        // Check palindrome
-        int original = n;
-        int reverse = 0;
-
-        while (n != 0) {
-            int digit = n % 10;
-            reverse = reverse * 10 + digit;
-            n = n / 10;
-        }
-
-        if (prime && original == reverse)
-            System.out.println(original + " is a Prime Palindrome");
-        else
-            System.out.println(original + " is not a Prime Palindrome");
-    }
+public class PrimePalindrome {
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ System.out.print("Enter a number: ");
+ int num = sc.nextInt();
+ int count = 0;
+ for (int i = 1; i <= num; i++) {
+ if (num % i == 0)
+ count++;
+ }
+ int temp = num;
+ int rev = 0;
+ while (temp > 0) {
+ int rem = temp % 10;
+ rev = rev * 10 + rem;
+ temp = temp / 10;
+ }
+ if (count == 2 && num == rev)
+ System.out.println("Prime Palindrome");
+ else
+ System.out.println("Not a Prime Palindrome");
+ sc.close();
+ }
 }
