464cd963602c911356b41d87b0dc7946c2340da9
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-11 11:03:26 +0530

    Create inputmissmatch.java

---
+++ ObservationProgram/exceptions/inputmissmatch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InputMismatchExample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
+
+            System.out.println("Your age is: " + age);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter an integer.");
+        }
+
+        sc.close();
+    }
+}
