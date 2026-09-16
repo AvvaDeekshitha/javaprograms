066b0ebed5ada2c80637583a2484a52b58031625
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-16 20:07:18 +0530

    Refactor ThrowDemo class to Throw and simplify exception

---
+++ ObservationProgram/exceptions/throw.java
@@ -0,0 +1,15 @@
+class Throw{
+    public static void main(String[] args) {
+        int age = 15;
+
+        try {
+            if (age < 18) {
+                throw new ArithmeticException( "Not eligible ");
+            }
+
+            System.out.println("Eligible");
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
