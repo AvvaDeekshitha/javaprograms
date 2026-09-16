3538043faf7de349f687a9702e969f8e7a1a8a7e
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-16 20:04:21 +0530

    Add StringExceptionDemo to handle index out of bounds

---
+++ ObservationProgram/exceptions/StringException.java
@@ -0,0 +1,10 @@
+class StringExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            String str = "Java";
+            System.out.println(str.charAt(10));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("String index is out of bounds");
+        }
+    }
+}
