5bbfaa9f6a6e82b1ed49011af9c983d688cb502a
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-16 20:03:29 +0530

    Rename class from NumberFormatExceptionDemo to NumberFormatException

---
+++ ObservationProgram/exceptions/NumberFormatException.java
@@ -0,0 +1,11 @@
+class NumberFormatException {
+    public static void main(String[] args) {
+        try {
+            String str = "abc";
+            int num = Integer.parseInt(str);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format");
+        }
+    }
+}
