03d8854a17821fa52fb2be7e9c08006fb1e2869a
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-24 19:22:23 +0530

    Add SortedSetDemo class with TreeSet example
    Implement a demo for using SortedSet with TreeSet.

---
+++ ObservationProgram/task five/treeset.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class SortedSetDemo {
+    public static void main(String[] args) {
+        SortedSet<Integer> set = new TreeSet<>();
+
+        set.add(40);
+        set.add(10);
+        set.add(30);
+        set.add(20);
+
+        System.out.println(set);
+        System.out.println("First: " + set.first());
+        System.out.println("Last: " + set.last());
+    }
+}
