6f2d947f4fe2aa8c98131a5c9e174bbf9d604cfd
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-24 18:18:37 +0530

    Add EvenOdd class to print even and odd numbers

---
+++ PracticePrograms/Arrays/Evenodd.java
@@ -0,0 +1,21 @@
+class EvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 22, 33, 40, 51};
+
+        System.out.println("Even numbers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+
+        System.out.println("Odd numbers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
