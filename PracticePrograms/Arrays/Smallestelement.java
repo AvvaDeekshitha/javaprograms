a6a4d3973a5008808d98ba52ab01d610a7862321
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-24 18:18:03 +0530

    Add SmallestElement class to find smallest number

---
+++ PracticePrograms/Arrays/Smallestelement.java
@@ -0,0 +1,15 @@
+class SmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {25, 10, 45, 30, 15};
+
+        int smallest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest = " + smallest);
+    }
+}
