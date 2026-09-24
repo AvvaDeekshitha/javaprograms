a62aca9c832f47c1c1f3379fc0e27920912b48e4
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-24 19:32:20 +0530

    Update setinterface.java

--- ObservationProgram/task five/setinterface.java
+++ ObservationProgram/task five/setinterface.java
@@ -1,15 +1,34 @@
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
 
 class SetDemo {
     public static void main(String[] args) {
+
         Set<Integer> set = new HashSet<>();
 
+        // add()
         set.add(10);
         set.add(20);
-        set.add(10);
         set.add(30);
+        set.add(20);
 
-        System.out.println(set);
-        System.out.println("Contains 20: " + set.contains(20));
+        System.out.println("Set: " + set);
+
+        // remove()
+        set.remove(20);
+        System.out.println("After remove: " + set);
+
+        // contains()
+        System.out.println("Contains 10: " + set.contains(10));
+
+        // size()
+        System.out.println("Size: " + set.size());
+
+        // isEmpty()
+        System.out.println("Is Empty: " + set.isEmpty());
+
+        // clear()
+        set.clear();
+        System.out.println("After clear: " + set);
     }
 }
