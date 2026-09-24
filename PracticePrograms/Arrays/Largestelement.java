3cedf815a097d2b7598a25ef3d9d680ff72c7555
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-24 18:17:31 +0530

    Implement LargestElement class to find max in array

--- PracticePrograms/Arrays/Largestelement.java
+++ PracticePrograms/Arrays/Largestelement.java
@@ -1,1 +1,15 @@
+class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {25, 10, 45, 30, 15};
 
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + largest);
+    }
+}
