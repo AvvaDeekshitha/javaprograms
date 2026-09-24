a4c188287d9afa5d80e72e39bc6327aabc85b52e
A25126552204 AVVADEEKSHITHA <avvadeekshitha.25.csm@anits.edu.in>
2026-09-24 18:20:34 +0530

    Add ArrayListExample class to demonstrate ArrayList usage

---
+++ PracticePrograms/Collections/Arraylists/Arraylist.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class ArrayListExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Ravi");
+        names.add("Anu");
+        names.add("Kiran");
+        names.add("Priya");
+
+        System.out.println("Names: " + names);
+    }
+}
